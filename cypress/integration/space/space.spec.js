1f235915c092816eefa70fa2c5cea9a6a9744a9f
edsonarios <edsonrios9@gmail.com>
2021-08-18 21:50:19 -0400

    add json for endpoints

--- cypress/integration/space/space.spec.js
+++ cypress/integration/space/space.spec.js
@@ -1,22 +1,24 @@
 const feature = require('../../clickup/api/features')
 const spaceJson = require('../../fixtures/space/space.json')
+const endpointSpace = require('../../fixtures/endpoint/space.json')
+const endpointTeam = require('../../fixtures/endpoint/team.json')
 describe('Create a Space with team', () => {
 
     let teamId = ''
     let spaceId = ''
     before(() => {
-        feature.getAll('/team').then((response) => {
+        feature.getAll(endpointTeam.team).then((response) => {
             teamId = response.body.teams[0].id
         })
     })
 
     it.only('should create it with necessary attributes', () => {
-        feature.create(`/team/${teamId}/space`, spaceJson).then((response) => {
+        feature.create(`${endpointTeam.team}/${teamId}${endpointSpace.space}`, spaceJson).then((response) => {
             spaceId = response.body.id
         })
     })
 
     afterEach(() => {
-        feature.deleteOne('/space', spaceId)
+        feature.deleteOne(endpointSpace.space, spaceId)
     })
 })
