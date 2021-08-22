46e40f3017043dbdb4d588b1e702e5f7b191d7a8
Juan Pablo Gonzales <juampi7237@gmail.com>
2021-08-22 11:51:27 -0400

    add create a space

--- cypress/clickup/api/teams/teamsFunctions.js
+++ cypress/clickup/api/teams/teamsFunctions.js
@@ -1,4 +1,4 @@
-const feature = require('../features');
+const feature = require('@api/features');
 const endpointTeam = require('@fixtures/endpoint/team.json')
 let teamId = ''
 export function getTeams() {
@@ -6,5 +6,8 @@ export function getTeams() {
         teamId = response.body.teams[0].id
         expect(response.status).to.eq(200);
         expect(response.body.teams.length).to.be.eq(1);
+        expect(response.body.teams[0]).to.have.all.keys(
+            'id', 'name', 'color', 'avatar', 'members'
+        )
     })
 }
--- cypress/integration/teams/teams.spec.js
+++ cypress/integration/teams/teams.spec.js
@@ -2,7 +2,7 @@
 const { getTeams } = require("@api/teams/teamsFunctions");
 // const {getTeams} = require("../../clickup/api/teams/teamsFunctions");
 
-describe("Temas endpoint", () => {
+describe("Teams endpoint", () => {
   it('Verify that the request "get team" we can obtain the list of teams and members of each team', () => {
     getTeams();
   });
