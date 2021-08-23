3c3058d03f59869a96917872cbfda94d9448209f
Juan Pablo Gonzales <juampi7237@gmail.com>
2021-08-23 08:13:05 -0400

    change int space methods

--- cypress/integration/space/space_create.spec.js
+++ cypress/integration/space/space_create.spec.js
@@ -3,22 +3,24 @@ const spaceJson = require('@fixtures/space/space.json')
 const endpointSpace = require('@fixtures/endpoint/space.json')
 const spaceErrorMessage = require('@fixtures/space/space_errors.json')
 const endpointTeam = require('@fixtures/endpoint/team.json')
-const { createSpace } = require('@api/spaces/spacesFunctions')
+const { createSpace,deleteSpace } = require('@api/spaces/spacesFunctions')
 
 describe('Test to Create Space', () => {
 
     let teamId = ''
     let spaceId = ''
-    let urlSpace = '';
-
+ 
     before(() => {
-        feature.getAll(endpointTeam.team).then((response) => {
-            teamId = response.body.teams[0].id
-        })
+        feature.getAll(endpointTeam.team)
+        .then((response) => teamId = response.body.teams[0].id)
     })
 
     it('Verify that the request "create space" and sending an object with the space name into the body we can create a space', () => {
-        createSpace(teamId).should((response)=>{
+        createSpace(teamId)
+        .then((response)=>{
+            console.log(response)
+            spaceid = response.body.id
+            console.log(spaceId)
             expect(response.status).to.eq(200);
             expect(response.body.name).to.be.eq(spaceJson.name);
             expect(response.body).to.have.all.keys(
@@ -30,7 +32,6 @@ describe('Test to Create Space', () => {
     it('Verify that the request "create space" and sending an object with repeat space name into the body we cannot create that space', () => {
         createSpace(teamId)
         .then((response) => {
-            console.log(response)
             expect(response.status).to.eq(400);
             expect(response.body.err).to.be.eq(spaceErrorMessage.errors.duplicate.err);
             expect(response.body).to.have.all.keys('err', 'ECODE')
@@ -38,6 +39,8 @@ describe('Test to Create Space', () => {
     })
 
     after(() => {
+        console.log(spaceId)
         feature.deleteOne(endpointSpace.space, spaceId)
+        // deleteSpace(spaceId)
     })
 })
